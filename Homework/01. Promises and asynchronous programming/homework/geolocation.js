1bb7ce1335b8f3295b1aaa79994fd60f8872ea4b
Stanimir Stoev <stoevs@gmail.com>
2016-09-23 00:00:40 +0300

    finished homework 1

--- Homework/01. Promises and asynchronous programming/homework/geolocation.js
+++ Homework/01. Promises and asynchronous programming/homework/geolocation.js
@@ -1,10 +1,31 @@
-(function (){
-navigator.geolocation.getCurrentPosition(function success(pos){  
-    console.log(pos);  
-}, function error(err) {
 
-})
+(function () {
+    
+    var myPromise = new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition((pos) => {
+            resolve(pos);
+        })
+    });
+    function parsePosition(pos) {
+        
+        return {
+            lat: pos.coords.latitude,
+            long: pos.coords.longitude,            
+        };
+    }
+    function displayMap(pos){              
+        var src = `http://maps.googleapis.com/maps/api/staticmap?center=${pos.lat},${pos.long}&zoom=17&size=500x500&sensor=false`;        
+        map = document.getElementById('map');
+        map.setAttribute('src', src);     
+        return map;    
+    }
 
+    
 
-
-}())
+    myPromise
+        .then(parsePosition)
+        .then(displayMap)      
+        .catch(function(error){
+            console.log(error);                        
+        });
+} ());
